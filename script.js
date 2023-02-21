77e491925920b9d1f9c5304467f1c1ce783e4889
IloveConchas <126017620+IloveConchas@users.noreply.github.com>
2023-02-21 11:37:35 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let button1 = document.querySelector(".button-one");           let button2 = document.querySelector(".button-two");           let opt1 = document.querySelector(".option-one-screen");  
+let opt2 = document.querySelector(".option-two-screen");
+
+button1.onclick = function() {
+    opt1.style.display = "block";
+    
+};
+button2.onclick = function() {
+    opt2.style.display = "block";
+};
+/* Declare variables below to save the different sections (divs) of your story*/
+
+
+
+// add oen or two more parts in the story where the trade is  Ica cream for a legandary pet. 
+
+// add the adopt me back ground 
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
